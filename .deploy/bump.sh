6e3418f223c7e00c527609f36d7fce7f3b2d93ed
Lucemans <luc@lucemans.nl>
2022-10-31 09:04:09 +0100

    Bump version to $version
    This is a new version of the package.

--- .deploy/bump.sh
+++ .deploy/bump.sh
@@ -2,9 +2,10 @@
 
 # Bump the version number in the package.json file
 version=$(git describe --tags | sed 's/^v//;s/\([^-]*-g\)/r\1/')
+version2=$(git describe --long --tags | sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g')
 
 # Update the version in the PKGBUILD file.
-sed -i "s/pkgver=.*/pkgver=$version/" PKGBUILD
+sed -i "s/pkgver=/pkgver=$version2/" PKGBUILD
 
 # Replace the version in the Cargo.toml file with the $version variable
 sed -i "0,/version = \".*\"/s//version = \"$version\"/" Cargo.toml
