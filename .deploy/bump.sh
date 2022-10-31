635b76c2232e93c8a0b6b1008fd96b03f055494d
Lucemans <luc@lucemans.nl>
2022-10-31 07:18:06 +0100

    Bump version to 0.1.4-pre1
    This is a pre-release of the next version of auto-commit.
    
    The version number has been bumped to 0.1.4-pre1.
    
    The version number has been changed to use a dash instead of a dot to
    separate the pre-release version from the main version.
    
    This is to make it easier to use the version number in the PKGBUILD file
    for the Arch Linux package.

--- .deploy/bump.sh
+++ .deploy/bump.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Bump the version number in the package.json file
-version=$(git describe --long --tags | sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g')
+version=$(git describe --long --tags | sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/-/g')
 
 # Update the version in the PKGBUILD file.
 sed -i "s/pkgver=/pkgver=$version/" PKGBUILD
