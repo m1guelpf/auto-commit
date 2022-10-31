053f12c2a944a69e551c72095eb0cb4b2d9a33a7
Lucemans <luc@lucemans.nl>
2022-10-31 06:22:58 +0100

    Bump version to 0.1.3
    This commit bumps the version number in the Cargo.toml file to 0.1.3.

--- .deploy/bump.sh
+++ .deploy/bump.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# This script is used to bump the version in the PKGBUILD file.
-
+# Bump the version number in the package.json file
 version=$(git describe --long --tags | sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g')
 
+# Update the version in the PKGBUILD file.
 sed -i "s/pkgver=/pkgver=$version/" PKGBUILD
+
+# Replace the version in the Cargo.toml file with the $version variable
+# sed -i "s/version = \".*\"/version = \"$version\"/" Cargo.toml
+
+# replace the first instance of version in the Cargo.toml file with the $version variable
+sed -i "0,/version = \".*\"/s//version = \"$version\"/" Cargo.toml
--- PKGBUILD
+++ PKGBUILD
@@ -1,5 +1,5 @@
 pkgname="auto-commit"
-pkgver=
+pkgver=0.1.3.r0.g88fc8030.1.3.r0.g88fc8030.1.3.r0.g88fc803
 pkgrel=1
 pkgdesc="A CLI tool that generates commit messages from your staged changes, built in Rust and using OpenAI's Codex."
 arch=("x86_64" "arm")
